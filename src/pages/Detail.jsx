a37d895539e3ffdd514a4b20b247a4c9af470227
Fajar <jarjaranugrah@gmail.com>
2023-10-28 11:33:05 +0700

    penambahan gambar dan detail

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 const Detail = () => {
   return (
     <View>
-      
-      <Image style={{height: 300, borderRadius: 10, marginLeft: 40, marginRight: 40, marginTop: 16, width: 400, alignItems: 'center', justifyContent: 'center' }}source={require('../assets/images/bebek.png')}/>
+    <Image style={{height: 300, borderRadius: 10, marginLeft: 40, marginRight: 40, marginTop: 16, width: 400, alignItems: 'center', justifyContent: 'center' }}source={require('../assets/images/bebek.png')}/>
       <Text style={{ fontWeight: 'bold', marginTop: 10, marginLeft: 40,   }}>Bebek Bumbu Ireng                                           Rp.24.000   </Text> 
       <Text style={{textAlign: 'center', marginLeft: 20, marginRight: 20, justifyContent:'space-between' }}>Bebek goreng dengan bumbu hitam khas madura terasa nikmat,
-       Codebluu pernah makan disini</Text> 
+       Codebluu pernah makan disini</Text>   
+      
        
 
        <View style={{ borderWidth: 0,
@@ -18,7 +18,7 @@ const Detail = () => {
        alignItems: 'center',
        backgroundColor: 'red',
        marginHorizontal: 16, marginTop: 290,
-       padding: 16,borderRadius: 40}}><TouchableOpacity onPress={() => navigation.navigate('Confirm')}></TouchableOpacity><Text style={{textAlign: 'center', marginLeft: 115 }}>Konfirmasi Pembayaran</Text></View>
+       padding: 16,borderRadius: 40}}><Text style={{textAlign: 'center', marginLeft: 115 }}>Konfirmasi Pembayaran</Text></View>
         
        
 
